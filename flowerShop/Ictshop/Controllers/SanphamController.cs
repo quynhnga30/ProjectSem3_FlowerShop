9441921fd28df7573c75f738e03a6925cda1732e
quynhnga30 <93904468+quynhnga30@users.noreply.github.com>
2023-04-02 20:17:23 +0700

    update

---
+++ flowerShop/Ictshop/Controllers/SanphamController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Controllers
+{
+    public class SanphamController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+        public ActionResult Index(string searchString)
+        {
+            var links = from l in db.Sanphams
+                        select l;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                links = links.Where(s => s.Tensp.Contains(searchString));
+            }
+
+            return PartialView(links);
+        }
+
+        // GET: Sanpham
+        public ActionResult womenday()
+        {
+            var ip = db.Sanphams.Where(n=>n.Mahang==2).Take(4).ToList();
+           return PartialView(ip);
+        }
+        public ActionResult motherday()
+        {
+            var ss = db.Sanphams.Where(n => n.Mahang == 1).Take(4).ToList();
+            return PartialView(ss);
+        }
+        public ActionResult valentine()
+        {
+            var mi = db.Sanphams.Where(n => n.Mahang == 3).Take(4).ToList();
+            return PartialView(mi);
+        }
+
+        public ActionResult listproducts()
+        {
+            var list = db.Sanphams.ToList();
+                
+            return PartialView(list);
+        }
+   
+
+        //public ActionResult dttheohang()
+        //{
+        //    var mi = db.Sanphams.Where(n => n.Mahang == 5).Take(4).ToList();
+        //    return PartialView(mi);
+        //}
+        public ActionResult xemchitiet(int Masp=0)
+        {
+            var chitiet = db.Sanphams.SingleOrDefault(n=>n.Masp==Masp);
+            if (chitiet == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(chitiet);
+        }
+
+    }
+
+}
